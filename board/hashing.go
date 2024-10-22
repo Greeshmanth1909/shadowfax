b4411a1c6164a25d322a5d7cb08ce779f38afe76
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2024-10-22 17:33:53 +0530

    position hashing

---
+++ board/hashing.go
@@ -0,0 +1,48 @@
+package board
+
+import (
+	"math/rand"
+)
+
+var PieceKeys [13][120]uint64
+
+var SideKey uint64
+
+var CastleKeys [16]uint64
+
+// InitHashKey initialises position keys with pseudo-random uint64 numbers
+func InitHashKey() {
+	for i := 0; i < 13; i++ {
+		for j := 0; j < 120; j++ {
+			PieceKeys[i][j] = rand.Uint64()
+		}
+	}
+	SideKey = rand.Uint64()
+	for i := 0; i < 16; i++ {
+		CastleKeys[i] = rand.Uint64()
+	}
+}
+
+// GenerateHash generates a Zobrist Hash for a given position
+func GenerateHash(pos *S_Board) uint64 {
+	var finalKey uint64 = 0
+	for i, piece := range *pos.Pieces {
+		if piece != NO_SQ && piece != EMPTY {
+			finalKey ^= piece
+		}
+	}
+	// TODO: Add logging and telemetry
+	if *pos.Side == WHITE {
+		finalKey ^= SideKey
+	}
+
+	if *pos.EnP != NO_SQ {
+		finalKey ^= PieceKeys[EMPTY][*pos.EnP]
+	}
+
+	if *pos.CastlePerm != 0 {
+		finalKey ^= CastleKeys[*pos.CastlePerm]
+	}
+
+	return finalKey
+}
