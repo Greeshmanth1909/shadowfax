fe2adbcdfc595446f6953c85dd08aa97691c412b
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2024-10-23 23:28:17 +0530

    add fen

---
+++ position/fen.go
@@ -0,0 +1,7 @@
+package position
+
+const StartPosition string = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
+func Parse_FEN(fen *string, brd *board.S_Board) error {
+	return nil
+}
