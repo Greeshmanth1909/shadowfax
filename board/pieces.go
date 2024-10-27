4b607ddb9e59cd7d4d1832fe2172b4525cfff0dc
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2024-10-28 00:38:42 +0530

    add piece arrays

--- board/pieces.go
+++ board/pieces.go
@@ -1,4 +1,8 @@
 package board
 
+// empty, pawn, bishop, night, rook, queen, king, pawn, bishop, night, rook, queen, king
 var BigPiece = [13]bool{false, false, true, true, true, true, true, false, true, true, true, true, true}
 var MajPiece = [13]bool{false, false, false, false, true, true, true, false, false, false, true, true, true}
+var MinPiece = [13]bool{false, false, true, true, false, false, false, false, true, true, false, false, false}
+var PieceVal [13]bool
+var PieceCol [13]bool
