0a2fbb316e19d027e007fff1d90f2493dbe47ee0
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2024-10-28 21:26:11 +0530

    add vals to arrs

--- board/pieces.go
+++ board/pieces.go
@@ -4,5 +4,5 @@ package board
 var BigPiece = [13]bool{false, false, true, true, true, true, true, false, true, true, true, true, true}
 var MajPiece = [13]bool{false, false, false, false, true, true, true, false, false, false, true, true, true}
 var MinPiece = [13]bool{false, false, true, true, false, false, false, false, true, true, false, false, false}
-var PieceVal [13]bool
-var PieceCol [13]bool
+var PieceVal = [13]int{0, 100, 325, 325, 550, 1000, 50000, 100, 325, 325, 550, 1000, 50000}
+var PieceCol = [13]Color{BOTH, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK}
