d1924262510df16927b50a7d0bfa6304620bd424
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2024-11-13 20:50:11 +0530

    set board to offboard before parsing fen

--- position/fen.go
+++ position/fen.go
@@ -16,6 +16,11 @@ Parse_FEN function parses the fen string and places the pieces in the board_stru
 	This function assumes valid fen strings
 */
 func Parse_FEN(fen *string, brd *board.S_Board) error {
+	// set all squares to offboard
+	for i := range brd.Pieces {
+		brd.Pieces[i] = board.Piece(board.OFFBOARD)
+	}
+
 	splitFen := strings.Split(*fen, " ")
 
 	if len(splitFen) != 6 {
