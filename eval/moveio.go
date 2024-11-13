10ac92e1cbd2a084ab865871a76b76512fa0b0b8
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2024-11-13 20:52:53 +0530

    add logic to print algebraic uci move to screen

---
+++ eval/moveio.go
@@ -0,0 +1,66 @@
+package eval
+
+import (
+	"fmt"
+	"github.com/Greeshmanth1909/shadowfax/board"
+	"log"
+	"strconv"
+)
+
+func PrintMove(mv *S_Move) {
+	frmSq := GetFromSquare(mv)
+	toSq := GetToSquare(mv)
+
+	if GetPromotedPiece(mv) == board.Piece(0) {
+		fmt.Printf("%v%v\n", ConvSq120ToAlge(frmSq), ConvSq120ToAlge(toSq))
+	} else {
+		fmt.Printf("%v%v%v\n", ConvSq120ToAlge(frmSq), ConvSq120ToAlge(toSq), GetPromotedPieceAlg(GetPromotedPiece(mv)))
+	}
+}
+
+// ConvSq120ToAlge function converts sq120 to corresponding algebraic notation i.e. 87 -> A7
+func ConvSq120ToAlge(sq board.Square) string {
+	sqInt := int(sq)
+	file := sqInt % 10
+	rank := (sqInt / 10) - 1
+	var algFile string
+	switch file {
+	case 1:
+		algFile = "a"
+	case 2:
+		algFile = "b"
+	case 3:
+		algFile = "c"
+	case 4:
+		algFile = "d"
+	case 5:
+		algFile = "e"
+	case 6:
+		algFile = "f"
+	case 7:
+		algFile = "g"
+	case 8:
+		algFile = "h"
+	default:
+		log.Fatalf("something went wrong (%v)", file)
+	}
+
+	strRnk := strconv.Itoa(rank)
+	return algFile + strRnk
+}
+
+// GetPromotedPieceAlg function returns algebraic notation of a given prompted, returns q (queen) by default
+func GetPromotedPieceAlg(p board.Piece) string {
+	switch p {
+	case board.Wk, board.Bk:
+		return "n"
+	case board.Wb, board.Bb:
+		return "b"
+	case board.Wq, board.Bq:
+		return "q"
+	case board.Wr, board.Br:
+		return "r"
+	default:
+		return "q"
+	}
+}
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/Greeshmanth1909/shadowfax/board"
-	_ "github.com/Greeshmanth1909/shadowfax/eval"
+	"github.com/Greeshmanth1909/shadowfax/eval"
 	"github.com/Greeshmanth1909/shadowfax/position"
 	"github.com/Greeshmanth1909/shadowfax/util"
 )
@@ -11,49 +11,22 @@ import (
 func main() {
 	util.InitAll()
 	fmt.Printf("%v\nStatus: running\n", board.Name)
-	fmt.Println("Printing bitboard")
-	var bb uint64 = 0
-	board.PrintBitBoard(bb)
-	board.SetBit(63, &bb)
-	board.PrintBitBoard(bb)
-	board.SetBit(15, &bb)
-	board.PrintBitBoard(bb)
-	fmt.Println("********")
-	board.ClearBit(15, &bb)
-	board.PrintBitBoard(bb)
 
 	startString := position.StartPosition
 	var boardStructure board.S_Board
-	for i := range boardStructure.Pieces {
-		boardStructure.Pieces[i] = board.Piece(board.OFFBOARD)
-	}
 	position.Parse_FEN(&startString, &boardStructure)
-
-	fmt.Println("******")
-	fmt.Println(boardStructure.Pieces[board.Square64to120[4]])
-	fmt.Println(boardStructure.Pieces[board.Square64to120[4]] == board.Bk)
-	fmt.Println(boardStructure.Pieces[board.Square64to120[28]] == board.EMPTY)
-
-	position.PrintBoard(&boardStructure)
-	fmt.Println("******")
-	startString = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
-	position.Parse_FEN(&startString, &boardStructure)
-	position.PrintBoard(&boardStructure)
-	fmt.Println("******")
-	startString = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
-	position.Parse_FEN(&startString, &boardStructure)
-	position.PrintBoard(&boardStructure)
-	startString = "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
-	position.Parse_FEN(&startString, &boardStructure)
-	position.PrintBoard(&boardStructure)
-
-	startString = "rnbq1rk1/ppp2ppp/3bpn2/3p4/3P4/2P2N2/PP2PPPP/RNBQKB1R w KQ - 2 7"
-	position.Parse_FEN(&startString, &boardStructure)
-	position.PrintBoard(&boardStructure)
-
-	//board.CheckBoard(&boardStructure)
 
 	startString = "8/8/8/4P3/8/2B5/8/8 w - - 0 1"
 	position.Parse_FEN(&startString, &boardStructure)
 	position.PrintBoard(&boardStructure)
+
+	fmt.Println(board.A7)
+
+	var move eval.S_Move
+	eval.SetFromSquare(&move, board.A7)
+	eval.SetToSquare(&move, board.B8)
+	eval.SetCapturedPiece(&move, board.Bk)
+	eval.SetPromotedPiece(&move, board.Wr)
+	eval.PrintMove(&move)
+
 }
