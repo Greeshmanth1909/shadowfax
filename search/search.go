511cad35a8ac4d9503b97e96df718b646f2cecef
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2024-12-18 12:01:00 +0530

    add alphabeta

--- search/search.go
+++ search/search.go
@@ -1,7 +1,10 @@
 package search
 
 import (
+	"fmt"
 	"github.com/Greeshmanth1909/shadowfax/board"
+	"github.com/Greeshmanth1909/shadowfax/eval"
+	"math"
 	"time"
 )
 
@@ -20,6 +23,31 @@ func IsRepetition(brd *board.S_Board) bool {
 }
 
 func SearchPositions(brd *board.S_Board, info *board.S_SearchInfo) {
+	var bestMove uint32
+	var bestScore int
+	var currentDepth int
+	// var pvMoves int
+	// var pvNum int
+
+	bestScore = int(math.Inf(-1))
+	ClearForSearch(brd, info)
+
+	for currentDepth = 0; currentDepth < info.Depth; currentDepth++ {
+		bestScore = AlphaBeta(int(math.Inf(-1)), int(math.Inf(1)), currentDepth, 1, brd, info)
+		// pvMoves = GetPvLine(currentDepth, brd)
+		bestMove = brd.PvArray[0]
+		fmt.Printf("score %v depth %v nodes %v\n", bestScore, currentDepth, info.Nodes)
+		fmt.Printf("move ")
+		var m eval.S_Move
+		m.Move = bestMove
+		eval.PrintMove(&m)
+		for i := 0; i < brd.PvTable.NumEntries; i++ {
+			val := brd.PvArray[i]
+			var mv eval.S_Move
+			mv.Move = val
+			eval.PrintMove(&mv)
+		}
+	}
 
 }
 
